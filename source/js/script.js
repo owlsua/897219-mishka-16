e559a9b9432ce50ff112e443acd3d9c980d9255f
aaaaaua <simferua@gmail.com>
2019-05-26 17:23:22 +0300

    Добавляет модальное окно, pixelperfect

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,10 @@
+focusWithin.polyfill(".focus-within")
+
+// ###MAIN-MENU-OPEN-CLOSE###
+
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -10,5 +15,42 @@ navToggle.addEventListener('click', function() {
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+  }
+});
+
+// ###MODAL-ORDER-OPEN-CLOSE###
+
+var links = document.querySelectorAll(".order-button");
+var popup = document.querySelector(".order-modal");
+var modalBackground = document.querySelector(".modal-background");
+
+var close = popup.querySelector(".order-modal__button");
+var form = popup.querySelector(".order-modal__form");
+
+for (var i=0; i < links.length; i++) {
+  links[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+    modalBackground.classList.add("modal-show");
+  });
+}
+
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  modalBackground.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      modalBackground.classList.remove("modal-show")
+    }
   }
 });
