0952f2b1616ec4817f49506fb6f2d124f197a75d
owlsua <69318224+owlsua@users.noreply.github.com>
2021-09-14 12:15:01 +0000

    Deploying to gh-pages from  @ 56d1024fdeb9250248aa17c27fa37cdc90effa66 🚀

---
+++ js/script.js
@@ -0,0 +1,59 @@
+
+if(focusWithin) {
+  focusWithin.polyfill(".focus-within");
+}
+
+// ###MAIN-MENU-OPEN-CLOSE###
+
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+// ###MODAL-ORDER-OPEN-CLOSE###
+
+var links = document.querySelectorAll(".order-button");
+var popup = document.querySelector(".order-modal");
+var modalBackground = document.querySelector(".modal-background");
+
+var close = popup.querySelector(".order-modal__button");
+var form = popup.querySelector(".order-modal__form");
+
+for (var i=0; i < links.length; i++) {
+  links[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+    modalBackground.classList.add("modal-show");
+  });
+}
+
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  modalBackground.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      modalBackground.classList.remove("modal-show")
+    }
+  }
+});
